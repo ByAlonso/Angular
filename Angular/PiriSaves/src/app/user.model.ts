e6b23711f4a31cae56f133595aa04cc1bce72e78
alonso <alonso.rapado@live.u-tad.com>
2019-05-18 13:29:10 +0200

    V1

---
+++ Angular/PiriSaves/src/app/user.model.ts
@@ -0,0 +1,4 @@
+export class User {
+	constructor(public username : String, public mail: String, public password: String, public token: String, public description: String,
+							public name: String, public image: String){}
+}
